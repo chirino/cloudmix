45925904f5ead575837346c6aed910b24747e858
James Strachan <james.strachan@gmail.com>
2009-07-28 12:56:13 +0000

    made the sample tests work as stand alone jars for now
    git-svn-id: http://fusesource.com/forge/svn/cloudmix/trunk@171 c32a53b6-b962-0410-9d7f-cad4864ec771

---
+++ bundles/org.fusesource.cloudmix.tests.producer/src/main/java/org/fusesource/cloudmix/tests/producer/Main.java
@@ -0,0 +1,51 @@
+/**************************************************************************************
+ * Copyright (C) 2009 Progress Software, Inc. All rights reserved.                    *
+ * http://fusesource.com                                                              *
+ * ---------------------------------------------------------------------------------- *
+ * The software in this package is published under the terms of the AGPL license      *
+ * a copy of which has been included with this distribution in the license.txt file.  *
+ **************************************************************************************/
+package org.fusesource.cloudmix.tests.producer;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * @version $Revision: 1.1 $
+ */
+public class Main {
+    public static void main(String[] args) {
+        if (args.length > 0 && args[0].equals("-debug")) {
+            Map<Object, Object> properties = new TreeMap<Object, Object>();
+            properties.putAll(System.getProperties());
+            for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+                System.out.println(" " + entry.getKey() + " = " + entry.getValue());
+            }
+        }
+
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("META-INF/spring/context.xml");
+        applicationContext.start();
+
+        System.out.println("Enter quit to stop");
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            while (true) {
+                String line = reader.readLine();
+                if (line == null || line.trim().equalsIgnoreCase("quit")) {
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Caught: " + e);
+            e.printStackTrace(System.err);
+        }
+
+        applicationContext.close();
+    }
+}
