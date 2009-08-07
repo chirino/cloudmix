3bfeb57b6c31731c6ef66bedfb7629d81aeb1470
James Strachan <james.strachan@gmail.com>
2009-08-07 13:37:44 +0000

    added reusable scala utils
    git-svn-id: http://fusesource.com/forge/svn/cloudmix/trunk@221 c32a53b6-b962-0410-9d7f-cad4864ec771

---
+++ org.fusesource.cloudmix.scalautil/src/main/scala/org/fusesource/cloudmix/scalautil/TextFormatting.scala
@@ -0,0 +1,40 @@
+package org.fusesource.cloudmix.scalautil
+
+import io.Source
+import _root_.scala.xml.parsing.XhtmlParser
+import _root_.scala.xml.{Text, NodeSeq}
+
+/**
+ * @version $Revision : 1.1 $
+ */
+object TextFormatting extends Logging {
+
+  def asText(value: AnyRef): String = {
+    if (value != null)
+      value.toString
+    else
+      ""
+  }
+
+  def asText(value: boolean): String = {
+    if (value)
+      "true"
+    else
+      "false"
+  }
+
+  def asMarkup(value: String): NodeSeq = {
+    // lets parse as markup
+    try {
+      log.info("parsing " + value)
+      
+      val source = Source.fromString("<p>" + value + "</p>");
+      XhtmlParser(source)
+    }
+    catch {
+      case e =>
+        log.warn("Failed to parse '" + value + "' due to " + e, e);
+        Text(value)
+    }
+  }
+}
