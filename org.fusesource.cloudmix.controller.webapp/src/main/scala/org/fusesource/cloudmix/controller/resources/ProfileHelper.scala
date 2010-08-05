73c0dc07e331cab85a7cfdee933abacd57a3fb60
James Strachan <james.strachan@gmail.com>
2010-08-05 16:26:30 +0100

    working profile view

--- org.fusesource.cloudmix.controller.webapp/src/main/scala/org/fusesource/cloudmix/controller/resources/ProfileHelper.scala
+++ org.fusesource.cloudmix.controller.webapp/src/main/scala/org/fusesource/cloudmix/controller/resources/ProfileHelper.scala
@@ -1,9 +1,11 @@
 package org.fusesource.cloudmix.controller.resources
 
-import org.fusesource.cloudmix.common.dto.ProfileDetails
 import org.fusesource.scalate.servlet.ServletRenderContext
+import org.fusesource.cloudmix.common.dto.{AgentDetails, DependencyStatus, FeatureDetails, ProfileDetails}
+import org.fusesource.cloudmix.common.URIs
 
 class ProfileHelper(implicit context: ServletRenderContext) {
+  // Profiles
 
   def profileLink(resource: ProfileResource): String = {
     profileLink(resource.getProfileDetails)
@@ -20,5 +22,46 @@ class ProfileHelper(implicit context: ServletRenderContext) {
   def propertiesLink(resource: ProfileResource): String = {
     profileLink(resource) + "/properties"
   }
+
+
+  // Features....
+
+  def featureLink(feature: FeatureDetails): String = {
+    featureLink(feature.getId)
+  }
+
+  def featureLink(feature: DependencyStatus): String = {
+    featureLink(feature.getFeatureId)
+  }
+
+  def featureLink(featureId: String): String = {
+    context.uri("/features/" + featureId)
+  }
+
+  def agentFeatureLink(agent: AgentDetails, featureId: String): String = {
+    URIs.appendPaths(agent.getHref, "features", featureId)
+  }
+
+  // Agents
+  def agentLink(agent: AgentDetails): String = {
+    agentLink(agent.getId)
+  }
+
+  def agentLink(agentId: String): String = {
+    context.uri("/agents/" + agentId)
+  }
+
+/*
+  def siteLink(agent: AgentDetails): NodeSeq = {
+    val href = agent.getHref
+    if (href != null)
+      <a href={href} class='site'>
+        {agent.getId}
+      </a>
+    else
+      Text("")
+  }
+*/
+
 
 }
